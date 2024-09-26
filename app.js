c5b4a05ee3a9bc96d0c61a05852932869ef65948
GarWox <gchin84@gmail.com>
2024-09-26 00:11:06 -0400

    Empezando a trabajar la app

---
+++ app.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import chalk from "chalk";
+
+async function getQuote(category) {
+  try {
+    let url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+  } catch (err) {}
+}
